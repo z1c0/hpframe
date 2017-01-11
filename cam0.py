14e0c0d96428090436d0cca76c546b2570c1c199
z1c0 <wziegler@live.at>
2017-01-11 20:14:46 +0000

    recording to mp4 now working

--- cam0.py
+++ cam0.py
@@ -1,15 +1,29 @@
+import os
+import subprocess
 from picamera import PiCamera
 from time import sleep
 
+fileName = '/home/pi/videos/good.h264'
+
 camera = PiCamera()
-#camera.start_preview()
-camera.start_recording('/home/pi/videos/hugo.h264')
-sleep(5)
+camera.resolution = (800, 1200)
+camera.brightness = 40
+camera.hflip = True
+camera.start_preview()
+camera.start_recording(fileName)
+camera.wait_recording(10)
 camera.stop_recording()
-#camera.stop_preview()
+camera.stop_preview()
+
+# conver to mp4
+fileNameMp4 = fileName + '.mp4';
+process = subprocess.Popen(['MP4Box', '-add', fileName, fileNameMp4])
+process.wait()
+os.remove(fileName)
+
+#MP4Box -fps 30 -add myvid.h264 myvid.mp4
 
 #omxplayer video.h264
-
 #camera.capture('/home/pi/Desktop/image.jpg')
 
 
